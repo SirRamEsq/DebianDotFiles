043e7c4918c413c0bb3dba089dabda471d5c91c6
SirRamESQ <gmrl95@gmail.com>
2019-09-09 14:12:45 -0400

    Tweaks

--- .scripts/xrandrSetup.sh
+++ .scripts/xrandrSetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#xrandr --output HDMI-0 --auto --left-of DVI-D-1
+xrandr --output HDMI-0 --auto --left-of DVI-D-1
 #xrandr --output DVI-D-1 --auto --left-of HDMI-0
 #xrandr --output DVI-D-1 --auto --right-of HDMI-0 --primary
 #xrandr --output HDMI-0 --primary --left-of DVI-D-0
-xrandr --output eDP-1 --mode 1920x1080
-xrandr --output eDP-1 --brightness 0.75
+#xrandr --output eDP-1 --mode 1920x1080
+#xrandr --output eDP-1 --brightness 0.75
